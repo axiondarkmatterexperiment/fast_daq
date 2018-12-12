6f3fa28cacca5bbc626e4dc4b82e88b4692cee7c
Ben LaRoque <benjamin.laroque@gmail.com>
2018-12-11 22:29:05 -0800

    adding a data type for power data

---
+++ source/data/power_data.hh
@@ -0,0 +1,31 @@
+/*
+ * power_data.hh
+ *
+ * Created on: Dec 11, 2018
+ *     Author: laroque
+ */
+
+#ifndef POWER_DATA_HH_
+#define POWER_DATA_HH_
+
+#include "member_variables.hh"
+
+namespace fast_daq
+{
+    class power_data
+    {
+        public:
+            power_data();
+            virtual ~power_data();
+
+        // member varaible macros
+        mv_accessible( double*, data_array );
+        mv_accessible( unsigned, array_size);
+
+        public:
+            void allocate_array( unsigned n_samples );
+
+    };
+} /* namespace fast_daq */
+
+#endif /* POWER_DATA_HH_ */
