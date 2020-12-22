91f71709661956686e9a7f415f6fbf7bd1f7c4ef
Raphael Cervantes <raphaelc@uw.edu>
2020-12-22 09:11:09 -0800

    changed a_master_config to a_primary_config to match psyllid

--- source/control/daq_control.cc
+++ source/control/daq_control.cc
@@ -26,7 +26,7 @@ namespace fast_daq
 {
     LOGGER( plog, "daq_control" );
 
-    daq_control::daq_control( const param_node& a_master_config, std::shared_ptr< sandfly::stream_manager > a_mgr ) :
+    daq_control::daq_control( const param_node& a_primary_config, std::shared_ptr< sandfly::stream_manager > a_mgr ) :
             sandfly::run_control( a_master_config, a_mgr ),
             f_use_monarch( true )
     {
