083639a43883d7862b36a5aa1c83074db29652b3
Raphael Cervantes <raphaelc@uw.edu>
2021-01-08 17:53:58 -0800

    removed spectrum_relay invocation of butterfly

--- source/daq/spectrum_relay.cc
+++ source/daq/spectrum_relay.cc
@@ -104,9 +104,6 @@ namespace fast_daq
         // grab the run description and load it into the broadcast payload
         scarab::param_ptr_t t_payload_ptr( new scarab::param_node() );
         scarab::param_node& t_payload = t_payload_ptr->as_node();
-        std::shared_ptr< daq_control > t_daq_control = std::dynamic_pointer_cast< daq_control >( use_run_control() );
-        scarab::param_input_json t_param_codec;
-        t_payload.merge( t_param_codec.read_string( t_daq_control->get_description() )->as_node() );
         scarab::param_array t_spectrum_array;
         for (unsigned i_bin=0; i_bin < a_spectrum->get_array_size(); ++i_bin)
         {
